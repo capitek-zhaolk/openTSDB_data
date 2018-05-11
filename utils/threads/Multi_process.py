49d4c1849217067407704247e2257abe76b47a8c
capitek-zhaolk <zhaolk@capitek.com.cn>
2018-05-11 14:28:09 +0800

    多进程

--- utils/threads/Multi_process.py
+++ utils/threads/Multi_process.py
@@ -28,55 +28,92 @@ import json
 import copy
 import random
 import simplejson
+import sys
 
-def send_json(jsons, s):
-    r = s.post("http://172.17.0.239:4242/api/put?details", json=jsons)
-    return r.text
+from time import sleep
+
+total = None
+# list保存响应时间
+res_times=[]
+
+# 数据库返回success、failed
+return_list = []
+
+def send_json(jsons):
+    with requests.Session() as session:
+        start_time_res = time.time()
+        r = session.post("http://172.17.0.239:4242/api/put?details", json=jsons, headers={'Connection':'close'})
+        return_list.append(r.text)
+        end_time_res = time.time()
+        res_times.append(end_time_res - start_time_res)
+        return r
 
 
 def testcase1(nums):
+    global process_name, total
+    total = 0
+    time_response = []
+    try:
 
-    process_name = multiprocessing.current_process().name
-    # print nums, process_name
-    s = requests.Session()
-    a = int(time.time())
-    json = {
-        "metric": "sys.batch.duoxiancheng",
-        "timestamp": a,
-        "value": int(5467829),
-        "tags": {
-            "name": "zhaolk_serverd27_2"
+        process_name = multiprocessing.current_process().name
+        a = int(time.time())
+        json = {
+            "metric": "sys.batch.duojincheng",
+            "timestamp": a,
+            "value": math.sin(nums) + 0.15,
+            "tags": {
+                "name": "zhaolk_mutil14"
+            }
         }
-    }
-    t = send_json(json, s)
+        t = send_json(json)
+        # t.elapsed.total_seconds()  # 获取响应时间
+        total = total + 1
+        time_response.append(t.elapsed.total_seconds())
+        return t.text, total, time_response
+    except Exception as e:
+        print e
+        sleep(2.5)
 
-    # print '名称是{0} 数据是{1}'.format(process_name, nums)
-    return t
+    finally:
+        pass
+
 
 if __name__ == '__main__':
-    total_nums = 222
-    thread_nums = 20
-    pool = multiprocessing.Pool(thread_nums)
+
+    total_nums = 10000
+    multi_process_nums =10
+    # multiprocessing.Pool 创建子进程
+    pool = multiprocessing.Pool(multi_process_nums)
 
     single_success_num = 0
     single_failed_num = 0
     success_num = 0
     failed_num = 0
+    total = 0
+    res_times = []
 
     start_time = time.time()
-    result = pool.map(testcase1, [i for i in range(1, total_nums+1)])
-    for idx, ele in enumerate(result):
-        ele = ele.encode('gbk')
-        ele = eval(ele)
-        for k, v in ele.items():
+
+    result = pool.map(testcase1, [i for i in range(0, total_nums)])
+
+    for ins in enumerate(result):
+        total += int(ins[1][1])
+        res_times.append(ins[1][2][0])
+
+        res = ins[1][0].decode('gb2312').encode('utf-8')
+        res_dict = eval(res)
+        for k, v in res_dict.items():
             if k == 'success':
                 single_success_num = int(v)
                 success_num += int(v)
             if k == 'failed':
                 single_failed_num = int(v)
                 failed_num += int(v)
+
     end_time = time.time()
-    print '发送{0}条数据，分为{1}个线程，耗时{2} sec , success {3}次 , failed {4}次 '.format(total_nums, thread_nums, end_time-start_time, success_num, failed_num)
+
+    print '在{0}秒内，一共访问HTTP Post方法{1}次，每秒访问{2}次，平均响应时间是{3} sec'.format(end_time-start_time, total, int(total / (end_time-start_time)), sum(res_times)/len(res_times))
+    print '发送{0}条数据，分为{1}个进程，耗时{2} sec , success {3}次 , failed {4}次 '.format(total_nums, multi_process_nums, end_time-start_time, success_num, failed_num)
     pool.close()
     pool.join()
 
